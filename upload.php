32743fddebe72fea82861a363155902ece070447
RoyLin <orangeskyonline@gmail.com>
2024-11-18 09:34:58 +0800

    新增檔案上傳表單

--- upload.php
+++ upload.php
@@ -19,6 +19,14 @@
 <body>
  <h1 class="header">檔案上傳練習</h1>
  <!----建立你的表單及設定編碼----->
+ <form action="manage.php" method="post" enctype="multipart/form-date">
+    <input type="file" name="file" id="file">
+    <input type="submit" value="上傳">
+
+
+
+
+ </form>
 
 
 
