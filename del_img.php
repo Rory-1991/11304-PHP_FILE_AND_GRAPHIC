0734e6c3bbabe98afa30a5d309f30b6c79dcc4c8
RoyLin <orangeskyonline@gmail.com>
2024-11-18 14:40:09 +0800

    完成刪除資料表紀錄及圖片檔案功能

--- del_img.php
+++ del_img.php
@@ -1,5 +1,10 @@
 <?php
+include_once "function.php";
+$id=$_GET['id'];
+$row=find('imgs',$id);
+$imgName=$row['filename'];
+// $imgName=find('imgs',$id)['filename']; 等於以上兩行寫法
 
-$imgName=$_GET['file'];
 unlink("./files/$imgName");
+del("imgs",$id); 
 header("location:manage.php");
